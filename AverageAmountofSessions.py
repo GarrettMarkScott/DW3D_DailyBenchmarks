774b951e62897bf3b7f38344b31556c2839c5b3d
Garrett Mark Scott <garrettmarkscott@gmail.com>
2020-04-02 21:28:58 -0400

    Created dynamic read/write variables

--- AverageAmountofSessions.py
+++ AverageAmountofSessions.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[179]:
-
 
 import sqlalchemy as db
 import matplotlib as mpl
@@ -10,9 +5,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import smtplib
 import configparser
+import path, os
+from pandas.plotting import register_matplotlib_converters
 
 
-# In[180]:
+
+pd.plotting.register_matplotlib_converters()
 
 
 SQL = '''
@@ -26,11 +24,10 @@ SQL = '''
     '''
 
 
-# In[181]:
-
-
 db_config = configparser.ConfigParser()
-db_config.read('dwdbconfig.ini')
+db_ini_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dwdbconfig.ini')
+print("The db ini path is looking here: " + db_ini_path)
+db_config.read(db_ini_path)
 db_host = db_config['mysql']['host']
 db_database = db_config['mysql']['database']
 db_user = db_config['mysql']['user']
@@ -38,27 +35,19 @@ db_pass = db_config['mysql']['password']
 db_port = db_config['mysql']['port']
 
 
-# In[182]:
-
 
 sql_alc_string = 'mysql+pymysql://'+db_user+':'+db_pass+'@'+db_host+':'+db_port+'/'+db_database
 print(sql_alc_string)
 
 
-# In[183]:
-
 
 engine = db.create_engine(sql_alc_string)
 
-
-# In[184]:
 
 
 connection = engine.connect()
 metadata = db.MetaData()
 
-
-# In[185]:
 
 
 df = pd.read_sql_query(SQL, engine)
@@ -68,20 +57,8 @@ raw_count = len(df.index)
 print(raw_count)
 
 
-# In[186]:
-
-
-df
-
-
-# In[187]:
-
-
 plt.figure(figsize=(20,30))
 plt.plot_date(x=df.index, y=df['Sessions']);
-
-
-# In[188]:
 
 
 std = df.std(skipna=True)[0]
@@ -90,52 +67,31 @@ upper = mean+std
 lower = mean-std
 
 
-# In[189]:
-
-
 df = df[df['Sessions'].between(lower,upper)]
 rows_in_1_std = len(df.index)
 print(str(round(rows_in_1_std/raw_count*100,1))+"% of the data is represented below after excluding data greater than 1 Standard Deviation from the mean")
 
 
-# In[190]:
-
-
-#What the "Clean Data" Looks like
-plt.figure(figsize=(20,30))
-plt.plot_date(x=df.index, y=df['Sessions']);
-
-
-# In[191]:
-
-
 weekly_totals = df.resample('W').mean()
 
-
-# In[192]:
-
-
+#Saving Weekly Session Averages to Local Machine
 plt.figure(figsize=(20,10))
 plt.plot(weekly_totals.index, weekly_totals['Sessions'])
 plt.xlabel('Date')
 plt.ylabel('Daily Average Sessions')
-plt.savefig('AvgClientSessions.png')
+plt.savefig(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'AvgClientSessions.png'))
 
+#Importing gmail credentials use GmailLogin.ini
 
-# In[193]:
-
-
-#securing password use GmailLogin.ini
+email_ini_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'GmailLogin.ini')
+print("The email ini path is looking here: " + email_ini_path)
 email_config = configparser.ConfigParser()
-email_config.read('GmailLogin.ini')
+email_config.read(email_ini_path)
 e_user = email_config['Gmail']['user']
 e_pass = email_config['Gmail']['password']
-#print(type(e_user))
 
 
-# In[195]:
-
-
+#Importing and sending email
 import smtplib, ssl
 from email import encoders
 from email.mime.text import MIMEText
@@ -143,16 +99,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 
 
-# In[196]:
-
-
 msg = MIMEMultipart('alternative')
-msg['Subject'] = "Average Daily Sessions"
+msg['Subject'] = "Weekly Summary Report"
 msg['From'] = e_user
-msg['To'] = 'garrettscott@mydealerworld.com'
-
-msg_content = MIMEText('This might be the one 2', 'plain', 'utf-8')
-msg.attach(msg_content)
+msg['To'] = 'garrettpythontest@gmail.com'
 
 
 # to add an attachment is just add a MIMEBase object to read a picture locally.
@@ -176,25 +126,5 @@ email_conn = smtplib.SMTP('smtp.gmail.com',587)
 email_conn.ehlo()
 email_conn.starttls() #encrypts password, needed for many connections
 email_conn.login('garrettmarkscott@gmail.com','mxqhsvwhlwzislxt') #need to generate app password from google
-email_conn.sendmail(e_user,'garrettscott@mydealerworld.com', msg.as_string())
+email_conn.sendmail(e_user,'garrettpythontest@gmail.com', msg.as_string())
 email_conn.quit()
-# server = smtplib.SMTP(smtp_server, 25)
-# server.set_debuglevel(1)
-# server.login(from_addr, password)
-# server.sendmail(from_addr, [to_addr], msg.as_string())
-# server.quit()
-
-
-# In[197]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
